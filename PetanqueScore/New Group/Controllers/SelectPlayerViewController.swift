45e9c260bfbf3883229c8810aed72f66bfca41ed
Benjamin Budet <benjamin.budet@epita.fr>
2018-04-20 12:39:36 +0200

    Add collectionView for victory selector

--- PetanqueScore/New Group/Controllers/SelectPlayerViewController.swift
+++ PetanqueScore/New Group/Controllers/SelectPlayerViewController.swift
@@ -32,7 +32,6 @@ class SelectPlayerViewController: UIViewController, UITableViewDelegate, UITable
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print(segue.identifier)
         if segue.identifier == "NewGame" {
             if let vc = segue.destination as? NewGameViewController {
                 vc.equipe1 = selectedPlayer
