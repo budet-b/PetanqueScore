0aaa21c2cf0c4cb2bfcffe702d45cd4eb97ec52e
Benjamin Budet <benjamin.budet@epita.fr>
2018-06-20 02:07:03 +0200

    [BUILD] Cleaned some code

--- PetanqueScore/New Group/Dashboard/Controller/DashboardViewController.swift
+++ PetanqueScore/New Group/Dashboard/Controller/DashboardViewController.swift
@@ -25,7 +25,6 @@ class DashboardViewController: UIViewController, WKUIDelegate, WKNavigationDeleg
         // Do any additional setup after loading the view.
         profilesButtonOutlet.layer.cornerRadius = 10
         profilesButtonOutlet.layer.borderWidth = 1
-
         self.locationManager.requestAlwaysAuthorization()
         self.locationManager.requestWhenInUseAuthorization()
         newGameOutlet.layer.cornerRadius = 10
@@ -34,27 +33,24 @@ class DashboardViewController: UIViewController, WKUIDelegate, WKNavigationDeleg
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
-            longit = (locationManager.location?.coordinate.latitude)!
-            latit = (locationManager.location?.coordinate.longitude)!
+            if let long = locationManager.location?.coordinate.longitude {
+                longit = long
+            }
+            if let lati = locationManager.location?.coordinate.latitude {
+                latit = lati
+            }
         }
-        
-        wkWebView = WKWebView(frame: CGRect(x: 0, y: self.view.frame.size.height - 50, width: self.view.frame.width, height: 50)
-, configuration: WKWebViewConfiguration())
+        wkWebView = WKWebView(frame: CGRect(x: 0, y: self.view.frame.size.height - 50, width: self.view.frame.width, height: 50) , configuration: WKWebViewConfiguration())
         wkWebView.uiDelegate = self
         wkWebView.navigationDelegate = self
         view.addSubview(wkWebView!)
         let scriptSource = "document.body.innerHTML = `Latitude: \(latit) Longitude \(longit)`;"
-
-        let jsScript = "var x = document.body; function getLocation() { if (navigator.geolocation) { navigator.geolocation.getCurrentPosition(showPosition); } else { x.innerHTML = `Geolocation is not supported by this browser.`; } } function showPosition(position) { x.innerHTML = `Latitude: ` + position.coords.latitude + `<br>Longitude: ` + position.coords.longitude; }"
         wkWebView.evaluateJavaScript(scriptSource, completionHandler: nil)
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        
         games = NSCodingData.GetGames() ?? []
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -63,7 +59,6 @@ class DashboardViewController: UIViewController, WKUIDelegate, WKNavigationDeleg
     }
     
     @IBAction func fjpjpActionClicked(_ sender: Any) {
-
         UIApplication.shared.open(URL(string : "https://www.ffpjp.org/")!, options: [:], completionHandler: nil)
     }
 
@@ -74,25 +69,6 @@ class DashboardViewController: UIViewController, WKUIDelegate, WKNavigationDeleg
     @IBAction func boulistenauteActionClicked(_ sender: Any) {
             UIApplication.shared.open(URL(string : "http://www.boulistenaute.com/")!, options: [:], completionHandler: nil)
     }
-    
-    func webView(_ webView: WKWebView,
-                 runJavaScriptAlertPanelWithMessage message: String,
-                 initiatedByFrame frame: WKFrameInfo,
-                 completionHandler: @escaping () -> Void) {
-        
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let title = NSLocalizedString("OK", comment: "OK Button")
-        let ok = UIAlertAction(title: title, style: .default) { (action: UIAlertAction) -> Void in
-            alert.dismiss(animated: true, completion: nil)
-        }
-        alert.addAction(ok)
-        present(alert, animated: true)
-        completionHandler()
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        wkWebView.evaluateJavaScript("alert('Hello from evaluateJavascript()')", completionHandler: nil)
-    }
     /*
      // MARK: - Navigation
      
@@ -102,5 +78,4 @@ class DashboardViewController: UIViewController, WKUIDelegate, WKNavigationDeleg
      // Pass the selected object to the new view controller.
      }
      */
-    
 }
